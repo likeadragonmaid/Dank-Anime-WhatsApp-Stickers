129c283eb3ad8cfbe01049a2095e9d0ab4d8b181
Karanvir Singh <karanvir.singh61@yahoo.in>
2019-07-19 13:26:37 +0530

    Migrate to AndroidX.

--- app/src/main/java/org/dynamicsoft/dankanimestickerswa/ContentFileParser.java
+++ app/src/main/java/org/dynamicsoft/dankanimestickerswa/ContentFileParser.java
@@ -8,7 +8,7 @@
 
 package org.dynamicsoft.dankanimestickerswa;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.JsonReader;
 
--- app/src/main/java/org/dynamicsoft/dankanimestickerswa/StickerPackValidator.java
+++ app/src/main/java/org/dynamicsoft/dankanimestickerswa/StickerPackValidator.java
@@ -11,7 +11,7 @@ package org.dynamicsoft.dankanimestickerswa;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Patterns;
